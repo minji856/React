34f6dc4018b6c195a7b52d874f33ee11b7268c17
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-20 16:50:00 +0900

    useCallback() & useMemo() React의 성능

--- 03_Component_2/Counter.tsx
+++ 03_Component_2/Counter.tsx
@@ -2,26 +2,40 @@ import * as React from 'react';
 import CountDisplay from './CountDisplay';
 import { useCounter } from './hooks/useCounter'; // default 아니여서 {} 로 가져오기
 import useToggle from './hooks/useToggle';
+import useWindowWidth from './hooks/useWindowWidth';
 
 interface CounterProps{
     title: string;
 }
 
-function Counter (props:CounterProps): React.ReactElement{
-
-    const {count, plus, minus} = useCounter();
+//function Counter (props:CounterProps): React.ReactElement{
+//const Counter = (props:CounterProps) => {
+//const Counter:React.FC<CounterProps> = (props) => {
+const Counter:React.FC<CounterProps> = ({title}) => {
+    console.log("첫번째 줄");
+    const {count, plus, minus, countLog, memoizedNumber} = useCounter();
+    console.log("useCount 다음")
     const [ hideCount, toggle ] = useToggle();
+    console.log("useToggle 다음")
 
     React.useEffect(()=>{console.log("[Rendered] Count / useEffect")});
     React.useEffect(()=>{console.log("[Mounted] Count / useEffect")}, []);
 
+    const width = useWindowWidth();
+
+    console.log("useEffect 다음");
+    countLog();
+
     return (
         <div>
-            <h1>{props.title} / {width}</h1>
+            {/* <h1>{props.title} / {width}</h1> */}
+            <h1>{title} / {width}</h1>
             {!hideCount && <CountDisplay count={count}/>}
             <button onClick={plus}>Plus</button>
             <button onClick={minus}>Minus</button>
             <button onClick={toggle}>Toggle Count</button>
+
+            <CountDisplay count= {memoizedNumber}/>
         </div>
     )
 }
--- 03_Component_2/hooks/useCounter.tsx
+++ 03_Component_2/hooks/useCounter.tsx
@@ -3,18 +3,36 @@ import * as React from 'react';
 
 interface useCounterReturnType{
     count: number,
-    plus: ()=> void;
-    minus: ()=> void;
+    plus: ()=> void,
+    minus: ()=> void,
+    countLog: ()=> void,
+    memoizedNumber :number;
 }
 
 export function useCounter (){
     const [count, setCount] = React.useState(0);
     
-    const plus = () => setCount((prev)=>prev + 1);
-    const minus = () => setCount((prev)=>prev - 1);
+    // const plus = () => setCount((prev)=>prev + 1);
+    const plus = React.useCallback (()=> setCount((prev)=>prev + 1), []);
+    // const minus = () => setCount((prev)=>prev - 1);
+    const minus = React.useCallback (()=> setCount((prev)=>prev - 1), []);
+    const countLog = React.useCallback (()=>console.log(count), []);
+
+
+    // useMemo 함수 만들기
+    const memoizedNumber = React.useMemo(()=>{
+        let result = count;
+        console.log("Calc Start");
+        for(let i=0; i<10000; i++){
+            console.log("result:", result);
+            result += i;
+        }
+        console.log("Calc Ends");
+        return result;
+    }, [count]);
     
     // 이때는 () 말고 {} 로 묶어주기
-    return { count, plus, minus};
+    return { count, plus, minus, countLog, memoizedNumber};
 }
 
 // export default useCounter; default 안 쓸꺼면 직접 함수에 적어주기
