9bfa25379c3abbbc64b2f05c0d9bf9beca072718
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-22 16:49:55 +0900

    Todo 기초

---
+++ 06_to-do-app/components/Todos.tsx
@@ -0,0 +1,15 @@
+
+const Todos: React.FC<{ items: string[] }> = (props) => {
+
+    return (
+        <ul>
+            {
+                props.items.map((item) => (
+                    <li>{item}</li>
+                ))
+            }
+        </ul>
+    );
+}
+
+export default Todos;
