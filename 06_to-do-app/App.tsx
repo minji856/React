684eb602a44a72177271008970694a2cf3c84a28
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-23 16:54:18 +0900

    item을 state에 저장하고 자식에게서 데이터 전달받기

--- 06_to-do-app/App.tsx
+++ 06_to-do-app/App.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import InsertTodo from './components/InsertTodo';
 import Todos from './components/Todos';
+import Todo from './utils/todos'
 
 function App() {
 
   // 할 일
-  const todos = []
+  // const todos = [new Todo("공부하기"), new Todo("청소하기")] 가짜테이더
+  const [ todos, setTodos ] = useState<Todo[]>([]);
   
+  const addTodoHandler = (item: string) => {
+    const newTodo = new Todo(item);
+
+    setTodos((prevTodos)=>{
+      return [...prevTodos, newTodo];
+    });
+  }
+
+  const removeTodoHandler = (id: string) => {
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
+  }
+
   return (
     <div>
-      <Todos items={["공부하기", "청소하기"]}/>
+      <InsertTodo onAddTodo={addTodoHandler}/>
+      <hr/>
+      <Todos items={todos} onRemoveTodo={removeTodoHandler}/>
     </div>
   );
 }
