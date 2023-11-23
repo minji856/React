4466dfd121352b3cff7e07c6193b771c2a39d88a
minji856 <144756912+minji856@users.noreply.github.com>
2023-11-23 16:39:02 +0900

    입력하면 밑에 리스트가 뜨고 그 리스트 누를 시 삭제 되게 EVENT

--- 06_to-do-app/components/Todos.tsx
+++ 06_to-do-app/components/Todos.tsx
@@ -1,11 +1,14 @@
+import Todo from '../utils/todos';
+import TodoItem from './TodoItem';
+import style from '../utils/Todos.module.css';
 
-const Todos: React.FC<{ items: string[] }> = (props) => {
-
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void}> = (props) => {
     return (
-        <ul>
+        <ul className={style.todos}>
             {
                 props.items.map((item) => (
-                    <li>{item}</li>
+                    <TodoItem text={item.text} id={item.id}
+                        onRemoveTodo = {props.onRemoveTodo}/>
                 ))
             }
         </ul>
